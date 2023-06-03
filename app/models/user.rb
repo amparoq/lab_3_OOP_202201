3ff4754c1439d32812ca99c41f389122a3d62bbe
amparoq <aquiroz@miuandes.cl>
2023-06-03 00:24:36 -0400

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :email, uniqueness: true
     validates :email, presence: true
     validate :only_one_administrator
